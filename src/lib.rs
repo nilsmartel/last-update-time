5f1466fc1c1dd784a50fa5d2763f9b45b54a86fb
Nils Martel <nilsmartel@yahoo.de>
2022-10-02 20:31:30 +0200

    Make logic more fault tollerant

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,50 @@
-use std::fs::{read_dir, DirEntry};
-use std::io::Result;
+use std::fs::read_dir;
 use std::time::SystemTime;
 
-pub fn last_update_time(path: &str) -> Result<SystemTime> {
+pub fn last_update_time(path: &str) -> Option<SystemTime> {
     // smallest thing I would ever need
     let mut smallest_time = SystemTime::UNIX_EPOCH;
 
-    for entry in read_dir(path)? {
-        let entry = entry?;
+    let dir = if let Ok(d) = read_dir(path) {
+        d
+    } else {
+        return None;
+    };
 
-        if let Ok(m) = entry.metadata() {
-            let mut time = m
-                .modified()
-                .unwrap_or_else(|_| m.created().expect("access modified or created time"));
+    for entry in dir {
+        if entry.is_err() {
+            continue;
+        }
+        let entry = entry.unwrap();
 
-            if m.is_dir() {
-                let path = entry.path();
-                let path = path.to_str().unwrap();
-                let subtime = last_update_time(path)?;
+        let m = entry.metadata();
+        if m.is_err() {
+            continue;
+        }
+        let m = m.unwrap();
+
+        let mut time = m
+            .modified()
+            .unwrap_or_else(|_| m.created().expect("read modified time or created time"));
+
+        if m.is_dir() {
+            let path = entry.path();
+            let path = path.to_str().unwrap();
+            if let Some(subtime) = last_update_time(path) {
                 if subtime < time {
                     time = subtime;
                 }
             }
+        }
 
-            if time < smallest_time {
-                smallest_time = time;
-            }
+        if time < smallest_time {
+            smallest_time = time;
         }
     }
 
-    Ok(smallest_time)
+    if smallest_time == SystemTime::UNIX_EPOCH {
+        return None;
+    }
+
+    Some(smallest_time)
 }
